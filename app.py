5c9971b037ed8e58ff444aa76152ac5db60b750e
yogeshraheja <yogesh.10d@gmail.com>
2021-12-17 12:10:27 +0530

    Created a simple app.py python file

---
+++ app.py
@@ -0,0 +1,10 @@
+import os
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def main():
+    return "Hello this page has been build using S2I feature of OS!"
+
+if __name__ == "__main__":
+     app.run(host="0.0.0.0", port=8080)
