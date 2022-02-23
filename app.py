f008d04426b538857d29bf9699edd75f01f53aa5
yogeshraheja <yogesh.10d@gmail.com>
2022-02-23 13:39:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return "Hello this page has been build using S2I feature of OS and update for testing!"
+    return "Hello this page has been build using S2I feature of OS and update for testing application!"
 
 if __name__ == "__main__":
      app.run(host="0.0.0.0", port=8080)
