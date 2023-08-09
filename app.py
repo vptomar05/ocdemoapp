1f12c2ece651e14170e3303e44f60c316b1b8f5a
vptomar05 <138429025+vptomar05@users.noreply.github.com>
2023-08-09 17:07:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 def main():
     return "Hello this page has been build using S2I feature of OpenShift and we are updating it for testing application!"
+    return "Hello this page has been build using S2I feature of OpenShift and we are updating it for testing application by master Asif sir!"
 
 if __name__ == "__main__":
      app.run(host="0.0.0.0", port=8080)
